7737b98a17d1f4c084da6c7d32290e402224aa08
sunda <sunda@ewaste.hu>
2013-02-05 10:55:13 +0100

    Start

---
+++ error_report.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<center>
+<p><h1>Hiba bejelentése</h1>
+
+<font size="4" color="#FF0000">Figyelem! Itt csak a hibákat jelentsd. Ha naptárat akarsz beküldeni, azt <a href="Naptarbekuldes.html">ide kattintva tedd meg.</a></font>
+</p><br>
+<form method="post">
+*Neved: <input type="text" name="Name" value="TesztElek" /><br>
+*e-mail(Ha szeretnéd hogy értesítsünk)<input type="email" name="Mail" /><br>
+Weboldalad(Ha van): <input type="text" name="Website" /><br>
+*Hiba leírása<input type="text" name="Error" value="ezt a hibát találtam" /><br>
+<button type="submit">Hiba jelentése</button><br>
+A *-gal jelölt mezők kitökltése kötelező.
+</form>
+
+<p>
+<b>Miért adjam meg a weboldalam?</b><br>
+Nem kötelező megadnod, de amennyiben az általad beküldött hibajelentést hasznosnak találjuk, a jelzésért cserébe megjelentetjük a weboldalad kattintható linkként.  
+</p>
+</center>
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,63 @@
+<html>
+<meta charset="UTF-8">
+<body>
+
+
+<div style="text-align: center"><h3>Hulladékszállítási naptár</h3></div>
+
+<h2><div style="text-align: center"><font color="#FF0000">6077 Orgovány</font></div></h2>
+
+
+<div style="text-align: center">Következő szállítás:</div>
+<div style="text-align: center; background-color:green">
+<font color="#FF0000"><h2>2013-02-10</h2></font>
+<font color="#FF0000">Kommunális hulladék</font></div>
+<br>
+<div style="text-align: center">>>>>>>Reklámblokk<<<<<<</div>
+<br>
+
+<p>
+<center> További hulladékszállítási dátumok:</center>
+
+<table align="center">
+
+<tr bgcolor="#FFFF00">
+<td><font color="#FF0000">2013-02-23</font></td>
+<td><font color="#FF0000">Szelektív</font></td>
+<td><font color="#FF0000">Kivételesen kedden az ünnepek miatt</font></td>
+</tr>
+<tr bgcolor="#CCFFFF">
+<td><font color="#FF0000">2013-02-29</font></td>
+<td><font color="#FF0000">Kommunális</font></td>
+<td></td>
+</tr>
+<tr bgcolor="#CCFFFF">
+<td><font color="#FF0000">2013-03-01</font></td>
+<td><font color="#FF0000">Kommunális</font></td>
+<td></td>
+</tr>
+<tr bgcolor="#FFFF00">
+<td><font color="#FF0000">2013-03-16</font></td>
+<td><font color="#FF0000">Szelektív</font></td>
+<td></td>
+</tr>
+
+</table>
+</font>
+</p>
+<p align="center">
+Segíts nekünk:<br>
+<a href="#">Hibát találtam a naptárban</a><br>
+<a href="#">Naptár hozzáadása</a>
+</p>
+<form>
+Hozzászólások a naptárhoz(később)
+</form>
+<br>
+<p align="center">Jelölések: 
+<br> Ami pirossal van az adatbázisból kerül betöltésre
+
+</p>
+
+</body>
+</html>
