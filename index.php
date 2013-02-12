fdd5e1d42cd6246a9c7dd5cb94d716d53de803a9
kissunda <dudds@ewaste.hu>
2013-02-12 14:28:31 +0100

    index->mobil és nagy nézet

--- index.php
+++ index.php
@@ -2,25 +2,33 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="css/index.css" />
+		<link rel="stylesheet" href="css/small.css" media="(max-width:480px)" />
 		<link rel="stylesheet" href="font/stylesheet.css" />
 	</head>
 <body>
 
 <header>
 	<h1>Hulladékszállítási naptárak</h1>
-	<h2>Magyarországi hulladékszállítási naptárai.</h2>
-	<p>Az oldal használata:</p>
-	<ol>
-	<li>Írd be az irányítószámod.</li>
-	<li>Kattints a Keresésre</li>
-	<li>Mentsd el a könyvjelzőid közé az oldalt.</li>
-</ol>
-<p>Tipp: Állítsd be a mobilodon a településed könyvjelzőként, hogy bármikor meg tudd nézni mikor viszik el tőletek a szemetet.</p>
+	<h2>Magyarország hulladékszállítási naptárai.</h2>
+	<ul>
+		<li><a href="#">s_Wastetype</a></li>
+		<li><a href="#">s_Wastetype2</a></li>
+	</ul>
 </header>
 
 <section>
+	<h1>Az oldal használata:</h1>
+	<ol>
+		<li><h1>1.</h1>Írd be az irányítószámod.<br><img alt="edit" src="css/edit.png" /></li>
+		<li><h1>2.</h1>Kattints a Keresésre.<br><img alt="search" src="css/search.png" /></li>
+		<li><h1>3.</h1>Mentsd el a könyvjelzőid közé.<br><img alt="bookmark" src="css/bookmark.png" /></li>
+	</ol>
+	<p class="tip">Tipp: Állítsd be a mobilodon a településed könyvjelzőként, hogy bármikor meg tudd nézni mikor viszik el tőletek a szemetet.</p>
+</section>
+
+<section>
 	<form method="get" target="_top">
-		<input type="text" id="postal" value="" name="postalcode" placeholder="Irányítószám" />
+		<input type="text" id="postal" value="" name="keres" placeholder="Irányítószám" />
 		<input type="submit" value="Keresés" />
 	</form>
 
@@ -30,8 +38,7 @@
 </section>
 
 <footer>
-	[s_WasteType/Name]:<br>
-	[s_WasteType/Description]<br>
+	ewaste.hu&copy; 2012
 </footer>
 
 </body>
